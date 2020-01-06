cfd37229c090fcdd95281e82035ed489c1fad146
David <david@theqoos.com>
2020-01-06 17:44:26 +0900

    firebase_auth init

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/david/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/david/theqoos/theqoos_hybrid/plugin/firebase_auth-0.15.1/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/david/flutter/bin/cache/artifacts/engine/ios"
